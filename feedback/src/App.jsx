5580859692512181e41356d08a222552401d9726
teosoph <39537001+teosoph@users.noreply.github.com>
2021-07-05 01:19:19 +0300

    HW-02___task-1 (Feedback)__step-2

--- feedback/src/App.jsx
+++ feedback/src/App.jsx
@@ -11,14 +11,34 @@ class App extends Component {
     bad: 0,
   };
 
+
+  // Increment function on buttons
   onBtnClick = nameValue => () => {
-
-    console.log(nameValue);
-
     this.setState(prevState => (
       { [nameValue]: prevState[nameValue] + 1 })
     );
   };
+
+  // Count total feedbacks function 
+  countTotalFeedback = () => {
+    const objValues = Object.values(this.state)
+    const totalFeedbacks = objValues.reduce((accum, value) => accum + value)
+    return (totalFeedbacks)
+  }
+
+  // Count Positive feedbacks function 
+  countPositiveFeedbackPercentage = () => {
+    const positiveFeedbacks = this.state['good']
+    console.log(positiveFeedbacks);
+
+    const totalPositiveFeedbacks = Math.round((positiveFeedbacks / this.countTotalFeedback()) * 100)
+    console.log(totalPositiveFeedbacks);
+
+    return (totalPositiveFeedbacks)
+  }
+
+
+
 
   render() {
     const { good, neutral, bad } = this.state;
@@ -27,6 +47,7 @@ class App extends Component {
 
     return (
       <div >
+
         <h1>
           Please leave feedback
       </h1>
@@ -40,9 +61,8 @@ class App extends Component {
               key={objKey}
               onClick={this.onBtnClick(objKey)}
             >
-
               {objKey}
-      
+
             </button>
           ))}
         </div>
@@ -50,9 +70,14 @@ class App extends Component {
         <h2>Statistic</h2>
 
         <ul className='statistic-list'>
+          {/* Statistic indicators */}
           <li className='statistic-item'>Good: {good}</li>
           <li className='statistic-item'>Neutral: {neutral}</li>
           <li className='statistic-item'>Bad: {bad} </li>
+
+          {/* Statistic methods */}
+          <li className='statistic-item'>Total: {this.countTotalFeedback()} </li>
+          <li className='statistic-item'>Positive feedbacks: {this.countPositiveFeedbackPercentage()} %</li>
         </ul>
 
       </div>
