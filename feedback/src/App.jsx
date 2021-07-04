2b086f5719b7a652686a40a4773afaa8d121fb4d
teosoph <39537001+teosoph@users.noreply.github.com>
2021-07-05 00:24:22 +0300

    HW-02___task-1

--- feedback/src/App.jsx
+++ feedback/src/App.jsx
@@ -4,36 +4,59 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  
+
   state = {
     good: 0,
     neutral: 0,
     bad: 0,
   };
 
+  onBtnClick = nameValue => () => {
+
+    console.log(nameValue);
+
+    this.setState(prevState => (
+      { [nameValue]: prevState[nameValue] + 1 })
+    );
+  };
+
   render() {
     const { good, neutral, bad } = this.state;
-    
+    const objKeys = Object.keys(this.state);
+
+
     return (
       <div >
         <h1>
           Please leave feedback
       </h1>
 
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+        <div>
+          {objKeys.map(objKey => (
+
+            <button
+              className='button'
+              type="button"
+              key={objKey}
+              onClick={this.onBtnClick(objKey)}
+            >
+
+              {objKey}
+      
+            </button>
+          ))}
+        </div>
 
         <h2>Statistic</h2>
 
-      <ul className='statistic-list'>
-          <li className='statistic-item'>Good: { good }</li>
-          <li className='statistic-item'>Neutral: { neutral }</li>
+        <ul className='statistic-list'>
+          <li className='statistic-item'>Good: {good}</li>
+          <li className='statistic-item'>Neutral: {neutral}</li>
           <li className='statistic-item'>Bad: {bad} </li>
         </ul>
-        
+
       </div>
-    
+
     );
   }
 }
