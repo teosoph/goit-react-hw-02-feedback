cca8a4eff1d3d000065b59408ab51a8483e87a20
teosoph <39537001+teosoph@users.noreply.github.com>
2021-07-07 01:14:26 +0300

    HW-02___task-1 (Feedback)__step-2 #fix-5

--- feedback/src/Components/FeedbackOptions/FeedbackOptions.jsx
+++ feedback/src/Components/FeedbackOptions/FeedbackOptions.jsx
@@ -8,7 +8,7 @@ export default function FeedbackOptions({ options, onLeaveFeedback }) {
       <button
         type="button"
         key={option}
-        onClick={onLeaveFeedback}
+        onClick={onLeaveFeedback(option)}
       >
         {option}
       </button>))}
